92c5098043031d1f4d036f266dbf2827fb5ea096
Matt Borgerson <contact@mborgerson.com>
2022-07-16 03:27:46 -0700

    Implement game_time_start

--- src/types.h
+++ src/types.h
@@ -1,13 +1,15 @@
 #include <stdint.h>
+#include <stddef.h>
 
 typedef unsigned int size_t;
 typedef unsigned char bool;
-typedef short wchar_t;
-
+static_assert(sizeof(wchar_t) == 2);
 static const bool true = 1;
 static const bool false = 0;
 
 #pragma pack(1)
+#define cs(s, e)    static_assert(sizeof(s) == e)
+#define co(s, f, o) static_assert(offsetof(s, f) == o)
 
 typedef struct {
     uint32_t unk_0;
@@ -17,12 +19,34 @@ typedef struct {
     char map_name[256];
 } game_options_t;
 
-static_assert(sizeof(game_options_t) == 0x10C);
+co(game_options_t, unk_0,      0);
+co(game_options_t, unk_4,      4);
+co(game_options_t, difficulty, 6);
+co(game_options_t, unk_8,      8);
+co(game_options_t, map_name,   12);
+cs(game_options_t, 0x10C);
 
-typedef union {
-    char unk[0x20];
+typedef struct {
+    bool initialized;
+    bool active;
+    char unk_2[10];
+    uint32_t local_time;
+    uint16_t elapsed;
+    char unk_17[6];
+    float speed;
+    uint32_t unk_28;
 } game_time_globals_t;
+#pragma pack()
 
-static_assert(sizeof(game_time_globals_t) == 0x20);
+co(game_time_globals_t, initialized, 0);
+co(game_time_globals_t, active,      1);
+co(game_time_globals_t, unk_2,       2);
+co(game_time_globals_t, local_time,  12);
+co(game_time_globals_t, elapsed,     16);
+co(game_time_globals_t, speed,       24);
+co(game_time_globals_t, unk_28,      28);
+cs(game_time_globals_t, 0x20);
 
+#undef co
+#undef cs
 #pragma pack()

f931e2f9118b3b309015de4d8333e966bcbf5516
Matt Borgerson <contact@mborgerson.com>
2022-07-17 02:14:23 -0700

    Implement data_initialize

--- src/halo/memory/data.c
+++ src/halo/memory/data.c
@@ -1,3 +1,20 @@
+void data_initialize(data_t *data, char *name, __int16 maximum_count,
+                     __int16 size)
+{
+  assert_halt(maximum_count > 0);
+  assert_halt(size > 0);
+  assert_halt(name);
+  assert_halt(data);
+
+  csmemset(data, 0, sizeof(*data));
+  csstrncpy(data->name, name, sizeof(data->name) - 1);
+  data->maximum_count = maximum_count;
+  data->size = size;
+  data->magic = 0x64407440;
+  data->data = &data[1];
+  data->valid = 0;
+}
+
 int data_allocation_size(__int16 count, __int16 size)
 {
   return size * count + sizeof(data_t);
@@ -7,6 +24,7 @@ void data_iterator_new(data_iter_t *iter, data_t *data)
 {
   data_verify(data);
   assert_halt(data->valid);
+
   iter->data = data;
   iter->cookie = (unsigned int)data ^ 0x69746572;
   iter->unk_4 = 0;

9da47acedcfc3a96e0b897722933fb1bb882ef9c
Matt Borgerson <contact@mborgerson.com>
2022-07-17 02:26:18 -0700

    Use sizeof(type) when allocating in players_initialize

--- src/halo/game/players.c
+++ src/halo/game/players.c
@@ -1,10 +1,12 @@
 void players_initialize(void)
 {
-    player_data = game_state_data_new("players", 16, 212);
-    team_data = game_state_data_new("teams", 16, 64);
-    players_globals = (players_globals_t *)game_state_malloc("players globals", NULL, 176);
-    csmemset(&players_globals->unk_0[4], -1, 0x10u);
-    *(_DWORD *)players_globals->unk_0 = -1;
-    *(_WORD *)&players_globals->unk_0[36] = 0;
-    player_control_globals = (player_control_globals_t *)game_state_malloc("player control globals", 0, 272);
+  player_data = game_state_data_new("players", 16, sizeof(player_data_t));
+  team_data = game_state_data_new("teams", 16, sizeof(team_data_t));
+  players_globals = (players_globals_t *)game_state_malloc(
+    "players globals", NULL, sizeof(players_globals_t));
+  csmemset(&players_globals->unk_0[4], -1, 0x10u);
+  *(_DWORD *)players_globals->unk_0 = -1;
+  *(_WORD *)&players_globals->unk_0[36] = 0;
+  player_control_globals = (player_control_globals_t *)game_state_malloc(
+    "player control globals", 0, sizeof(player_control_globals_t));
 }

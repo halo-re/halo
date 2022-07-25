aea224378a1bae69267be9afd0aa8241b0f7a1b6
Matt Borgerson <contact@mborgerson.com>
2022-07-24 18:42:25 -0700

    Rename rasterizer_preinitialize

--- src/halo/shell_xbox.c
+++ src/halo/shell_xbox.c
@@ -3,7 +3,7 @@ int main(int argc, const char **argv, const char **envp)
 #if DEBUG_BUILD
   update_loaded_module_section_attributes();
 #endif
-  rasterizer_preinitialize__fill_you_up_with_the_devils_cock();
+  rasterizer_preinitialize();
   physical_memory_allocate();
   if (shell_initialize()) {
     main_loop();

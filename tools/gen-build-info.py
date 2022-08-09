50027e2383f7f3876d97c6fbd264874e8f9e502a
wutno <aaron@installgentoo.net>
2022-08-09 00:15:06 -0400

    tools: Add git rev to BUILD_REV

--- tools/gen-build-info.py
+++ tools/gen-build-info.py
@@ -2,8 +2,15 @@ import subprocess
 from datetime import datetime
 
 def main():
+	git_rev = "UNKNOWN"
+	try:
+		run = subprocess.run(["git", "describe", "--always", "--dirty"], stdout=subprocess.PIPE)
+		if run.stdout.strip() != "":
+			git_rev = run.stdout.strip().decode("utf-8")
+	except:
+		pass
 	print(f'''
-#define BUILD_REV "0000000"
+#define BUILD_REV "{ git_rev }"
 #define BUILD_DATE "{ datetime.utcnow().isoformat() }"
 const char *build_rev = BUILD_REV;
 const char *build_date = BUILD_DATE;
@@ -12,3 +19,4 @@ const char *build_ui_widget_text = "HDP " BUILD_REV;
 
 if __name__ == '__main__':
 	main()
+

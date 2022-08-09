cb909215daa7439cdac3b8fd38cf77ae390d6a48
Matt Borgerson <contact@mborgerson.com>
2022-08-09 02:34:28 -0700

    display_assert: Improve style and accuracy

--- src/halo/cseries/cseries.c
+++ src/halo/cseries/cseries.c
@@ -4,9 +4,6 @@ void display_assert(const char *reason, const char *filepath, int lineno,
   if (halt) {
     stack_walk(0);
   }
-  if (!reason) {
-    reason = "<no reason given>";
-  }
   error(2, "EXCEPTION %s in %s,#%d: %s", halt ? "halt" : "warn", filepath,
-        lineno, reason);
+        lineno, reason ? reason : "<no reason given>");
 }

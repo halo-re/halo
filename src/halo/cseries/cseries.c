6927321a34fa40873d50affcfcd62f20ffd6069c
wutno <aaron@installgentoo.net>
2022-08-11 22:16:51 -0400

    cseries: Create csstrlen

--- src/halo/cseries/cseries.c
+++ src/halo/cseries/cseries.c
@@ -7,3 +7,18 @@ void display_assert(const char *reason, const char *filepath, int lineno,
   error(2, "EXCEPTION %s in %s,#%d: %s", halt ? "halt" : "warn", filepath,
         lineno, reason ? reason : "<no reason given>");
 }
+
+size_t __cdecl csstrlen(const char *s1)
+{
+  assert_halt(s1);
+
+  size_t size = 0;
+
+  while (s1[size]) {
+        ++size;
+  }
+
+  assert_halt(size >= 0 && size < MAXIMUM_STRING_SIZE);
+
+  return size;
+}

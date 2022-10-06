ad944a3817b36e20d63ad6363b3dd9fc7e7bbad1
Ernegien <mdavis.virtual@gmail.com>
2022-10-05 23:18:00 -0500

    Add broken players_initialize_for_new_map

--- src/halo/game/players.c
+++ src/halo/game/players.c
@@ -10,3 +10,20 @@ void players_initialize(void)
   player_control_globals = (player_control_globals_t *)game_state_malloc(
     "player control globals", 0, sizeof(player_control_globals_t));
 }
+
+void players_initialize_for_new_map(void)
+{
+  player_control_dispose();
+  csmemset(&players_globals, 0,  sizeof(players_globals_t));
+  csmemset(&players_globals->local_player_indices[0], -1, 0x10u);
+  csmemset(&players_globals->local_dead_player_indices[0], -1, 0x10u);
+  players_globals->unk_0 = -1;
+  players_globals->input_disabled = false;
+  players_globals->double_speed_ticks_remaining = 0;
+  players_globals->are_all_dead = false;
+  players_globals->unk_tag_index = -1;
+  players_globals->respawn_failure = 0;
+  data_make_valid(player_data);
+  data_make_valid(team_data);
+  csmemset(&unk_46B668, -1, 64);
+}

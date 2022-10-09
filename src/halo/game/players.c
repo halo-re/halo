d79b40d38a1cc27904f046cbc3a6e2df277af336
Ernegien <mdavis.virtual@gmail.com>
2022-10-08 19:07:16 -0500

    wip

--- src/halo/game/players.c
+++ src/halo/game/players.c
@@ -4,7 +4,7 @@ void players_initialize(void)
   team_data = game_state_data_new("teams", 16, sizeof(team_data_t));
   players_globals = (players_globals_t *)game_state_malloc(
     "players globals", NULL, sizeof(players_globals_t));
-  csmemset(&players_globals->local_player_indices[0], -1, 0x10u);
+  csmemset(&players_globals->local_players[0], -1, sizeof(datum_index_t) * MAXIMUM_NUMBER_OF_LOCAL_PLAYERS);
   players_globals->unk_0 = -1;
   players_globals->local_player_count = 0;
   player_control_globals = (player_control_globals_t *)game_state_malloc(
@@ -15,8 +15,8 @@ void players_initialize_for_new_map(void)
 {
   player_control_dispose();
   csmemset(players_globals, 0,  sizeof(players_globals_t));
-  csmemset(&players_globals->local_player_indices[0], -1, 0x10u);
-  csmemset(&players_globals->local_dead_player_indices[0], -1, 0x10u);
+  csmemset(&players_globals->local_players[0], -1, sizeof(datum_index_t) * MAXIMUM_NUMBER_OF_LOCAL_PLAYERS);
+  csmemset(&players_globals->local_dead_players[0], -1, sizeof(datum_index_t) * MAXIMUM_NUMBER_OF_LOCAL_PLAYERS);
   players_globals->unk_0 = -1;
   players_globals->input_disabled = false;
   players_globals->double_speed_ticks_remaining = 0;
@@ -25,7 +25,7 @@ void players_initialize_for_new_map(void)
   players_globals->respawn_failure = 0;
   data_make_valid(player_data);
   data_make_valid(team_data);
-  csmemset(&unk_46B668, -1, 64);
+  csmemset(&unk_46B668, -1, 16 * MAXIMUM_NUMBER_OF_LOCAL_PLAYERS);
 }
 
 void players_dispose_from_old_map(void)
@@ -98,9 +98,9 @@ int find_unused_local_player_index()
   return v2;
 }
 
-void player_delete(int player_datum)
+void player_delete(datum_index_t player)
 {
-  datum_delete(player_data, player_datum);
+  datum_delete(player_data, player);
 }
 
 int16_t players_get_respawn_failure(void)
@@ -117,7 +117,160 @@ int local_player_get_player_index(int16_t local_player_index)
   if (local_player_index == NONE)
     player_index = NONE;
   else
-    player_index = players_globals->local_player_indices[local_player_index];
+    player_index = players_globals->local_players[local_player_index].handle;
 
   return player_index;
 }
+
+void local_player_set_player_index(int16_t local_player_index, datum_index_t index)
+{
+  assert_halt(local_player_index>=0 && local_player_index<MAXIMUM_NUMBER_OF_LOCAL_PLAYERS);
+
+  if (players_globals->local_players[local_player_index].handle != -1) {
+    player_data_t* player = datum_get(player_data, players_globals->local_players[local_player_index]);
+    player->local_player_index = -1;
+  }
+
+  players_globals->local_players[local_player_index] = index;
+  if (index.handle != -1)
+  {
+    player_data_t* player = datum_get(player_data, players_globals->local_players[local_player_index]);
+    player->local_player_index = local_player_index;
+  }
+}
+
+int16_t local_player_count(void)
+{
+  return players_globals->local_player_count;
+}
+
+datum_index_t local_player_get_next(datum_index_t local_player)
+{
+  datum_index_t next_player = INVALID_DATUM_HANDLE;
+  datum_index_t *player = &players_globals->local_players[0];
+
+  for (int i=0; i<MAXIMUM_NUMBER_OF_LOCAL_PLAYERS; i++) {
+    if (player->handle != -1 && i > local_player.index && (i < next_player.index || next_player.index == -1)) {
+        next_player.handle = i;
+    }
+    player++;
+  }
+
+  return next_player;
+}
+
+// TODO: player_index_from_unit_index
+// TODO: player_died
+
+bool players_are_all_dead(void)
+{
+  return players_globals->are_all_dead;
+}
+
+// TODO: players_set_local_player_unit
+
+void* players_get_combined_pvs_local(void)
+{
+  return players_globals->combined_pvs_local;
+}
+
+void* players_get_combined_pvs(void)
+{
+  return players_globals->combined_pvs;
+}
+
+void player_input_enable(bool enabled)
+{
+  players_globals->input_disabled = enabled == 0;
+}
+
+bool player_input_enabled(void)
+{
+  return players_globals->input_disabled == 0;
+}
+
+// TODO: any_player_is_in_the_air
+
+bool any_player_is_dead(void)
+{
+  player_data_t* player;
+  data_iter_t iter;
+
+  data_iterator_new(&iter, player_data);
+  player = data_iterator_next(&iter);
+
+  if (!player)
+    return false;
+
+  while (player->slave_unit_index.handle != -1)
+  {
+    player = data_iterator_next(&iter);
+    if ( !player )
+      return false;
+  }
+
+  return true;
+}
+
+// TODO: is_player_in_trigger
+// TODO: player_pseudo_kill
+// TODO: player_control_fix_for_loaded_game_state
+// TODO: player_get_starting_location_count
+// TODO: player_get_starting_location
+// TODO: placement_data_set_change_color
+// TODO: create_weapon
+// TODO: players_compute_combined_pvs
+
+int players_compute_local_player_count()
+{
+  int count = 0;
+
+  for (int i=0; i<MAXIMUM_NUMBER_OF_LOCAL_PLAYERS; i++) {
+    if (players_globals->local_players[i].handle != -1)
+      count++;
+  }
+
+  return count;
+}
+
+void player_examine_nearby_unit()
+{
+
+}
+
+// TODO: unit_should_autopick_weapon
+// TODO: player_reset_action_result (eax = player datum index)
+// TODO: player_handle_weapon_swap
+// TODO: player_over_shield_screen_effect
+// TODO: player_active_camo_screen_effect
+// TODO: player_health_pack_screen_effect
+// TODO: player_powerup_on
+// TODO: player_powerup_additional
+// TODO: player_powerup_off
+// TODO: players_handle_deleted_object
+// TODO: direction3d random_direction3d(int seed);
+// TODO: valid_real_vector2d
+// TODO: player_new
+// TODO: player_add_equipment
+// TODO: player_aiming_vector_from_facing
+// TODO: player_teleport_internal
+// TODO: player_teleport
+// TODO: find_best_starting_location_index
+// TODO: player_spawn
+// TODO: player_set_action_result
+// TODO: player_handle_action
+// TODO: player_handle_powerup
+// TODO: player_handle_powerup_minor
+// TODO: player_update_powerups
+// TODO: players_debug_render
+// TODO: debug_player_teleport
+// TODO: players_respawn_coop
+// TODO: player_teleport_on_bsp_switch
+// TODO: players_reconnect_to_structure_bsp
+// TODO: player_examine_nearby_vehicle
+// TODO: player_examine_nearby_device
+// TODO: player_handle_powerup_equipment
+// TODO: players_update_before_game
+// TODO: player_examine_nearby_item
+// TODO: player_examine_nearby_objects
+// TODO: players_update_after_game
--- src/types.h
+++ src/types.h
@@ -41,6 +41,16 @@ static const bool false = 0;
 
 #pragma pack(1)
 
+typedef union {
+  int32_t handle;
+  struct {
+    int16_t index;
+    int16_t salt;
+  };
+} datum_index_t;
+
+static const datum_index_t INVALID_DATUM_HANDLE = { -1 };
+
 /// size=0x10c
 typedef struct {
   uint32_t unk_0;         ///< offset=0x00
@@ -78,9 +88,125 @@ typedef struct {
 
 /// size=0xd4
 typedef struct {
-  char unk0[2];
+  char unk0[2]; // salt? and this is actually a datum index?
   int16_t local_player_index;
-  char unk_0[0xd0];
+
+  // .text:000AC599                 add     ecx, 4
+  // .text:000BB398                 mov     word ptr [esi+1Ah], 0 ; null terminator
+  wchar_t player_name[12];  
+
+  int32_t unk_28; // .text:000BB3A8                 mov     [esi+1Ch], edi
+
+  // .text:00085A85                 cmp     [eax+20h], esi  datum_index_t team? or raw index - .text:000B1719                 mov     ecx, dword_456BA8[eax*4]
+  // .text:000AD07B                 mov     ax, [edx+20h]
+  // .text:000BB3B9                 mov     dword ptr [esi+20h], 1
+  int32_t unk_32;
+
+  int32_t unk_36; // .text:000BAEC6                 mov     dword ptr [eax+24h], 0FFFFFFFFh
+  int16_t unk_40; // .text:000BAEC0                 mov     word ptr [eax+28h], 0
+  int16_t unk_42; // .text:000BC0C1                 mov     [ebx+2Ah], ax
+  int16_t unk_44; // .text:000A8CF3                 mov     eax, [esi+2Ch]
+
+  int16_t unk_46;
+
+  int32_t unk_48; // .text:000AF707                 mov     edx, [esi+30h]
+  datum_index_t slave_unit_index; // mov     eax, [esi+34h] 
+  datum_index_t last_slave_unit_index;  // .text:00085C51                 mov     eax, [eax+38h]
+  
+  // .text:000BAD03                 mov     [esi+3Ch], ax
+  // .text:000BB951                 mov     word ptr [edi+3Ch], 0FFFFh
+  int16_t unk_60;
+  int16_t unk_62; // .text:000BD4CD                 mov     [edx+3Eh], al
+  int32_t unk_64; // .text:000BB3AF                 mov     [esi+40h], edi
+
+  char unk_68[36];
+
+  // .text:000BBF8E                 lea     ecx, [edi+68h]
+  // .text:000BC391                 cmp     word ptr [ebx+esi*2+68h], 0
+  // .text:000BC404                 add     [ebx+esi*2+68h], dx
+  // .text:000D12A4                 movsx   ecx, word ptr [edi+6Ah]
+  int16_t unk_104;
+  int16_t unk_106;
+
+  // .text:000A81F8                 mov     dword ptr [eax+6Ch], 3F800000h  speed for score
+  // .text:000B4C27                 fld     dword ptr [esi+6Ch]
+  // .text:001A5A9C                 fld     dword ptr [eax+6Ch]
+  float unk_108;
+  
+  // .text:000A81FF                 mov     [eax+70h], ecx
+  // .text:000AD696                 mov     dword ptr [esi+70h], 0FFFFFFFFh
+  uint32_t unk_112;
+
+  // .text:000A998C                 mov     [eax+74h], edx
+  // .text:000A81F2                 mov     [eax+74h], ecx
+  uint32_t unk_116;
+
+  // .text:000A998F                 mov     [eax+78h], ecx
+  // .text:000A81F5                 mov     [eax+78h], ecx
+  uint32_t unk_120;
+
+  uint32_t unk_124; // .text:000A8202                 mov     [eax+7Ch], ecx
+  uint32_t unk_128; // .text:000AC424                 mov     ecx, [esi+80h]
+  uint32_t unk_132; // .text:000A8BFA                 mov     eax, [eax+84h]
+
+  // .text:000B3916                 mov     dword ptr [eax+88h], 0
+  // .text:000B4B26                 mov     dword ptr [eax+88h], 0FFFFFFFFh
+  uint32_t unk_136; 
+
+  uint16_t unk_140;
+
+  uint16_t unk_142; // .text:000B56A3                 mov     word ptr [esi+8Eh], 1
+  uint16_t unk_144; // .text:000B56AE                 inc     word ptr [esi+90h]
+  uint16_t unk_146; // .text:000B575C                 mov     [eax+92h], si
+  uint16_t unk_148; // .text:000B576C                 mov     [eax+94h], si
+  uint16_t unk_150; // .text:000B5763                 mov     word ptr [eax+96h], 0FFFFh
+  uint16_t unk_152; // .text:000ABE0C                 movsx   edx, word ptr [ebx+98h]
+
+  char unk_154[6];
+
+  uint16_t unk_160; // .text:000ABE16                 movsx   eax, word ptr [ebx+0A0h]
+
+  char unk_162[6];
+
+  uint16_t unk_168; // .text:000B58E6                 inc     word ptr [esi+0A8h]
+
+  // .text:00086E9C                 cmp     [eax+0AAh], bx
+  // .text:000A8C6A                 movsx   edx, word ptr [eax+0AAh]
+  uint16_t unk_170;
+
+  uint16_t unk_172; // .text:000B5749                 inc     word ptr [eax+0ACh]
+
+  char unk_174[10];
+
+  uint32_t unk_184; // .text:000B569D                 mov     [esi+0B8h], edx
+
+  uint32_t unk_188;
+
+  // .text:000AE80F                 add     eax, 0C0h memset size of 4 (array of two words?)
+  // .text:000B166F                 inc     word ptr [esi+0C0h]
+  // .text:000B0F78                 inc     word ptr [esi+0C2h]
+  uint16_t unk_192;
+  uint16_t unk_194;
+
+  // .text:000B008D                 inc     word ptr [edi+0C4h]
+  // .text:000B04FD                 movsx   eax, word ptr [eax+0C4h]  score
+  uint16_t unk_196;
+
+  uint16_t unk_198; // padding?
+
+  uint32_t unk_200; // .text:000BDC78                 mov     eax, [esi+0C8h]
+
+  // .text:000A71F0                 mov     eax, [esi+0CCh]
+  // .text:000C9AD0                 cmp     [eax+0CCh], edi
+  uint32_t unk_204;
+
+  bool unk_208; // .text:000BDC70                 mov     al, [esi+0D0h]
+
+  // .text:000A71FB                 mov     cl, [esi+0D1h]
+  // .text:000A720F                 mov     byte ptr [esi+0D1h], 1
+  bool unk_209;
+
+  uint16_t unk_210;
 } player_data_t;
 
 /// size=0x40
@@ -90,19 +216,19 @@ typedef struct {
 
 /// size=0xb0
 typedef struct {
-  int unk_0;                                                        //< offset=0x00 see local_player_get_next, players_compute_local_player_count
-  int local_player_indices[MAXIMUM_NUMBER_OF_LOCAL_PLAYERS];        //< offset=0x04
-  int local_dead_player_indices[MAXIMUM_NUMBER_OF_LOCAL_PLAYERS];   //< offset=0x14
-  int16_t local_player_count;                                       //< offset=0x24
-  int16_t double_speed_ticks_remaining;                             //< offset=0x26
-  bool are_all_dead;                                                //< offset=0x28
-  bool input_disabled;                                              //< offset=0x29
-  int16_t unk_tag_index;                                            //< offset=0x2A bsp index? see players_reconnect_to_structure_bsp pulling from tag block at scnr+0x39C
-  int16_t respawn_failure;                                          //< offset=0x2C enum, see _players_get_respawn_failure
-  bool teleported;                                                  //< offset=0x2E
-  char unk_flags;                                                   //< offset=0x2F bsp-related flags? see players_update_after_game
-  char combined_pvs[0x40];                                          //< offset=0x30
-  char combined_pvs_local[0x40];                                    //< offset=0x70
+  int unk_0;                                                         //< offset=0x00 see local_player_get_next, players_compute_local_player_count
+  datum_index_t local_players[MAXIMUM_NUMBER_OF_LOCAL_PLAYERS];      //< offset=0x04
+  datum_index_t local_dead_players[MAXIMUM_NUMBER_OF_LOCAL_PLAYERS]; //< offset=0x14
+  int16_t local_player_count;                                        //< offset=0x24
+  int16_t double_speed_ticks_remaining;                              //< offset=0x26
+  bool are_all_dead;                                                 //< offset=0x28
+  bool input_disabled;                                               //< offset=0x29
+  int16_t unk_tag_index;                                             //< offset=0x2A bsp index? see players_reconnect_to_structure_bsp pulling from tag block at scnr+0x39C
+  int16_t respawn_failure;                                           //< offset=0x2C enum, see _players_get_respawn_failure
+  bool teleported;                                                   //< offset=0x2E
+  char unk_flags;                                                    //< offset=0x2F bsp-related flags? see players_update_after_game
+  char combined_pvs[0x40];                                           //< offset=0x30
+  char combined_pvs_local[0x40];                                     //< offset=0x70
 } players_globals_t;
 
 /// size=0x110
@@ -195,12 +321,12 @@ typedef struct {
 /// size=0xAC
 typedef struct
 {
-  __int16           player;      ///< offset=0x00
-  bool              unk_2;       ///< offset=0x02
-  char              unk_3[129];  ///< offset=0x03
-  viewport_bounds_t unk_132;     ///< offset=0x84
-  viewport_bounds_t unk_140;     ///< offset=0x8c
-  char              unk_148[24]; ///< offset=0x94
+  __int16           local_player_index; ///< offset=0x00
+  bool              unk_2;              ///< offset=0x02
+  char              unk_3[129];         ///< offset=0x03
+  viewport_bounds_t unk_132;            ///< offset=0x84
+  viewport_bounds_t unk_140;            ///< offset=0x8c
+  char              unk_148[24];        ///< offset=0x94
 } window_t;
 
 // FIXME: Structure size

2bd05d77e45fba7e2504c4de2f5a45221161a999
Ernegien <mdavis.virtual@gmail.com>
2022-10-14 19:06:13 -0500

    Implement player_died

--- src/halo/game/players.c
+++ src/halo/game/players.c
@@ -175,7 +175,26 @@ datum_index_t player_index_from_unit_index(datum_index_t unit_index)
   return player_index;
 }
 
-// TODO: player_died
+void player_died(datum_index_t player_index)
+{
+  player_data_t* player = datum_get(player_data, player_index);
+  player->last_slave_unit_index = player->slave_unit_index;
+  player->slave_unit_index = INVALID_DATUM_HANDLE;
+
+  uint16_t local_player_index = player->local_player_index;
+  if (local_player_index != -1) {
+    player_control_new_unit(local_player_index, INVALID_DATUM_HANDLE);
+  }
+
+  players_globals->are_all_dead = true;
+
+  data_iter_t iter;
+  data_iterator_new(&iter, player_data);
+  for (player_data_t* player = data_iterator_next(&iter); player; player = data_iterator_next(&iter)) {
+    if (player->slave_unit_index.handle != -1)
+      players_globals->are_all_dead = false;
+  }
+}
 
 bool players_are_all_dead(void)
 {
